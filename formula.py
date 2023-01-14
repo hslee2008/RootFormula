67b8e75edb24b1d04d5c73d38c354f3e79e02e5c
Hyunseung Lee <hyunseunglee2008@gmail.com>
2023-01-14 21:31:12 +0900

    Create formula.py

---
+++ formula.py
@@ -0,0 +1,49 @@
+# Formula m√x
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from decimal import *
+
+getcontext().prec = 999
+m = int(input("Enter m: "))
+x = int(input("Enter x: "))
+repeat = int(input("Enter repeat: "))
+
+def NormalRoot():
+  return (x ** (1 / m))
+
+xpoints = []
+ypoints = []
+NormYPoints = NormalRoot()
+
+def HyunseungRootFormula():
+  sum = 0
+
+  for i in range(repeat):
+    power = pow(np.log(x) / m ,i)
+
+    sum += Decimal(power) / Decimal(math.factorial(i))
+
+    xpoints.append(i)
+    ypoints.append(sum)
+
+  return sum
+
+def graph():
+  plt.plot(np.array(xpoints), np.array(ypoints), color="r", label="Hyunseung Root Formula")
+  plt.axhline(y=NormalRoot(), color='b', linestyle='dotted')
+  plt.show()
+
+def main():
+  norm = NormalRoot()
+  hyun = HyunseungRootFormula()
+
+  print("Normal Root: ", norm)
+  print("Hyunseung Root: ", hyun)
+  print("Error: " + str(abs(Decimal(norm) - hyun)))
+
+  graph()
+
+# main
+main()
